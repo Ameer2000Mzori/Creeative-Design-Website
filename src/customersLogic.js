8bb6113f3926fe74011c5d2f19c7e968fd3bc8bf
Ameer <ameerapex496@gmail.com>
2024-01-15 07:18:31 +0100

    added bottom side of show customers card

--- src/customersLogic.js
+++ src/customersLogic.js
@@ -21,6 +21,40 @@ class ShowCustomersCard {
     this.img = img;
     this.customersName = customersName;
   }
+  showData() {
+    // creating elements
+    const customersCard = document.createElement("div");
+    customersCard.classList.add("customers-Card");
+
+    // adding data dynamiclly
+    customersCard.innerHTML = `
+    <div class="customers-Text-Wrap">
+    <i id="customers-Card-Text-Icon" class="fas fa-quote-left"></i>
+    <h1 class="customers-Card-Text-2">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure
+      dolorem hic, tempora provident optio consectetur non
+      laudantium mollitia, similique assumenda adipisci in fugiat.
+    </h1>
+  </div>
+  <div class="customers-Rate-Img-Name-Wrap">
+    <div class="customers-Rate-Wrap">
+      <i id="customers-Icon-Star" class="fa-solid fa-star"></i>
+      <i id="customers-Icon-Star" class="fa-solid fa-star"></i>
+      <i id="customers-Icon-Star" class="fa-solid fa-star"></i>
+      <i id="customers-Icon-Star" class="fa-solid fa-star"></i>
+      <i id="customers-Icon-Star" class="fa-regular fa-star"></i>
+    </div>
+    <img
+      class="customers-Card-Profile-Img"
+      src="./assets/customer-img-1.jpg"
+      alt=""
+    />
+    <h2 class="customers-Card-Name">Jane Lee</h2>
+  </div>`;
+
+    // appending data
+    customersBottomCardsWrap.appendChild(customersCard);
+  }
 }
 
 // functions
