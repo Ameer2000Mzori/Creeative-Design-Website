cb330ba02e7a87f635bcee922fa56a5716c866fd
Ameer <ameerapex496@gmail.com>
2024-01-15 07:16:08 +0100

    added upper side of show customers card

--- src/customersLogic.js
+++ src/customersLogic.js
@@ -9,11 +9,19 @@ const DataObject = [
     Deco: "fas fa-quote-left",
     text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure dolorem hic, tempora provident optio consectetur non laudantium mollitia, similique assumenda adipisci in fugiat.",
     img: "./assets/customer-img-1.jpg",
-    name: "Jane Lee",
+    customersName: "Jane Lee",
   },
 ];
 
 // class
+class ShowCustomersCard {
+  construction(Deco, text, img, customersName) {
+    this.Deco = Deco;
+    this.text = text;
+    this.img = img;
+    this.customersName = customersName;
+  }
+}
 
 // functions
 
