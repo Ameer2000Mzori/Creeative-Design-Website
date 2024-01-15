236e45b280e9bc2160ebf400b65e7f7d99e82ae7
Ameer <ameerapex496@gmail.com>
2024-01-15 07:13:05 +0100

    selected elements + created data object

--- src/customersLogic.js
+++ src/customersLogic.js
@@ -1,6 +1,17 @@
 // selecting elements
+const customersBottomCardsWrap = document.getElementsByClassName(
+  "customers-Bottom-Cards-Wrap"
+)[0];
 
 // object
+const DataObject = [
+  {
+    Deco: "fas fa-quote-left",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure dolorem hic, tempora provident optio consectetur non laudantium mollitia, similique assumenda adipisci in fugiat.",
+    img: "./assets/customer-img-1.jpg",
+    name: "Jane Lee",
+  },
+];
 
 // class
 
