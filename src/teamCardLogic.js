b4f822ba59622cd2fcf6f6e7f26cc3fe06180d58
Ameer <ameerapex496@gmail.com>
2024-01-16 07:41:21 +0100

    added comments for better reading

--- src/teamCardLogic.js
+++ src/teamCardLogic.js
@@ -1,9 +1,13 @@
 // selecting elements
-const teamCard = document.getElementsByClassName("team-Card")[0];
-const teamCardBtn = document.getElementsByClassName("team-Card-Btn")[0];
-const teamCardBtnLine =
-  document.getElementsByClassName("team-Card-Btn-Line")[0];
-const teamCardPage2 = document.getElementsByClassName("team-Card-Page-2")[0];
+
+// cards
+const teamCard = document.querySelectorAll("team-Card");
+const teamCardPage2 = document.querySelectorAll("team-Card-Page-2");
+
+// buttons
+const teamCardBtn = document.querySelectorAll("team-Card-Btn");
+const teamCardBtnLine = document.querySelectorAll("team-Card-Btn-Line");
+
 // global varibales
 
 // functions
