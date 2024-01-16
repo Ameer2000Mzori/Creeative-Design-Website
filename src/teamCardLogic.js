75b858b2f59d7ba95c82e9a2d225d9913a1c2cb0
Ameer <ameerapex496@gmail.com>
2024-01-16 07:40:11 +0100

    selected elements needed for the function

--- src/teamCardLogic.js
+++ src/teamCardLogic.js
@@ -1,5 +1,9 @@
 // selecting elements
-
+const teamCard = document.getElementsByClassName("team-Card")[0];
+const teamCardBtn = document.getElementsByClassName("team-Card-Btn")[0];
+const teamCardBtnLine =
+  document.getElementsByClassName("team-Card-Btn-Line")[0];
+const teamCardPage2 = document.getElementsByClassName("team-Card-Page-2")[0];
 // global varibales
 
 // functions
