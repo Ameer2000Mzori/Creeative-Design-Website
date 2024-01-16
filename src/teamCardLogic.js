c287aaaa3cdb39611ab0853c4ca1be069db35ced
Ameer <ameerapex496@gmail.com>
2024-01-16 08:42:36 +0100

    clean up code and small changes

--- src/teamCardLogic.js
+++ src/teamCardLogic.js
@@ -1,7 +1,5 @@
 // selecting elements
 
-// Add a click event listener to the parent element
-
 // cards
 const teamCard = document.querySelectorAll(".team-Card");
 const teamCardPage2 = document.querySelectorAll(".team-Card-Page-2");
@@ -13,7 +11,7 @@ const teamCardBtnLine = document.querySelectorAll(".team-Card-Btn-Line");
 // global variables
 
 // functions
-const openUserCard = (e, index) => {
+const openUserCard = (index) => {
   // remove all classes function call
   removeAllClasses();
 
@@ -21,10 +19,6 @@ const openUserCard = (e, index) => {
   teamCard[index].classList += " active";
   teamCardPage2[index].classList += " active";
   teamCardBtnLine[index].classList += " active";
-
-  // test console
-  console.log(teamCard);
-  console.log(teamCard[index]);
 };
 
 // remove all classes function
@@ -47,7 +41,7 @@ const removeAllClasses = () => {
 
 // events
 teamCardBtns.forEach((cardBtn, index) => {
-  cardBtn.addEventListener("click", (e) => openUserCard(e, index));
+  cardBtn.addEventListener("click", () => openUserCard(index));
 });
 
 window.addEventListener("click", (e) => {
