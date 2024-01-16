8b501b8aa98caa19620fb038f7a767280ffbf923
Ameer <ameerapex496@gmail.com>
2024-01-16 05:20:55 +0100

    added object with arrys of data

--- src/teamLogic.js
+++ src/teamLogic.js
@@ -1,6 +1,23 @@
 // selecting elements
+const teamBottomCardsWrap = document.getElementsByClassName(
+  "team-Bottom-Cards-Wrap"
+)[0];
 
 // global varibales
+const teamDataObj = {
+  images: [
+    "./assets/team-member-1.jpg",
+    "./assets/team-member-2.jpg",
+    "./assets/team-member-3.jpg",
+  ],
+  name: ["Nick Smith", "Bob Brown", "John Doe"],
+  role: ["Designer", "Designer", "Developer"],
+  aboutText: [
+    ["Ps", "Figma", "HTML5", "CSS3", "Ai"],
+    ["Ae", "Pr", "HTML5", "CSS3", "Inkscape"],
+    ["JavaScript", "ReactJS", "NodeJS", "MongoDB", "Python"],
+  ],
+};
 
 // Class
 
