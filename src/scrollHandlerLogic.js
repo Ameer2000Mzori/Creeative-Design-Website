71208b579b1de1f1392ecc06da3d33dfb793bc83
Ameer <ameerapex496@gmail.com>
2024-01-17 15:40:59 +0100

    added new event and new function called user scroll

--- src/scrollHandlerLogic.js
+++ src/scrollHandlerLogic.js
@@ -4,5 +4,11 @@ const scrollUpBtn = document.getElementsByClassName("scroll-Up-Btn")[0];
 // gelobal varibales
 
 // functions
+const userScroll = () => {
+  if (window.scrollY) {
+    console.log(window.screenY);
+  }
+};
 
 // events
+window.addEventListener("scroll", userScroll);
